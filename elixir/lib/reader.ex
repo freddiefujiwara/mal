b4d6f2e8cf99c90575742473771604c067aeb0f7
Fumikazu Fujiwara <github@ze.gs>
2015-06-14 20:34:45 +0900

    peek next

--- elixir/lib/reader.ex
+++ elixir/lib/reader.ex
@@ -1,10 +1,28 @@
 defmodule Reader do
-    def tokenize(str) do
+    def peek(obj) do
+        word = Enum.at obj.tokens, obj.position
+        if word == nil do
+            {obj,nil}
+        else
+            {obj,word}
+        end
+    end
+    def next(obj) do
+        word = Enum.at obj.tokens, obj.position
+        if word == nil do
+            {obj,nil}
+        else
+            {Dict.put(obj, :position, obj.position + 1),word}
+        end
+    end
+    def tokenize(obj,str) do
         tokenize_regex = ~r/[\s,]*(~@|[\[\]{}()'`~^@]|"(?:\\.|[^\\"])*"|;.*|[^\s\[\]{}('"`,;)]*)/
-        Enum.filter(
+        obj = Dict.put(obj, :tokens, Enum.filter(
             Enum.map(
                 Regex.scan(tokenize_regex, str), 
-                fn(x) -> String.strip(List.first x) end),
-            fn(x) -> String.length(x) > 0 end)
+                fn(x) -> String.strip(List.first x) end),#["  token ","  token "] to "token"
+            fn(x) -> String.length(x) > 0 end)           #no empty
+        )
+        obj
     end
 end
--- elixir/test/reader_test.exs
+++ elixir/test/reader_test.exs
@@ -1,9 +1,53 @@
 defmodule ReaderTest do
   use ExUnit.Case
 
-  test "READ" do
-    assert Reader.tokenize("str") == ["str"]
-    assert Reader.tokenize("(str)") == ["(","str",")"]
-    assert Reader.tokenize("(+ 1 (* 2 3))") == ["(","+","1","(","*","2","3",")",")"]
+  test "Reader.tokenize" do
+    reader = %{:tokens => [], :position => 0}
+
+    reader = Reader.tokenize(reader,"str")
+    assert reader.tokens == ["str"]
+
+    reader = Reader.tokenize(reader,"(str)")
+    assert reader.tokens == ["(","str",")"]
+
+    reader = Reader.tokenize(reader,"(+ 1 (* 2 3))")
+    assert reader.tokens == ["(","+","1","(","*","2","3",")",")"]
+  end
+  test "Reader.next and Reader.peek" do
+    reader = %{:tokens => [], :position => 0}
+
+    reader = Reader.tokenize(reader,"(+ 1 (* 2 3))")
+
+    {reader,word} = Reader.peek(reader)
+    assert word == "("
+    assert reader.position == 0
+    {reader,word} = Reader.next(reader)
+    assert word == "("
+    assert reader.position == 1
+    {reader,word} = Reader.peek(reader)
+    assert word == "+"
+    assert reader.position == 1
+
+    {reader,_} = Reader.next(reader)
+    {reader,_} = Reader.next(reader)
+    {reader,_} = Reader.next(reader)
+    {reader,_} = Reader.next(reader)
+    {reader,_} = Reader.next(reader)
+    {reader,_} = Reader.next(reader)
+    {reader,_} = Reader.next(reader)
+
+    {reader,word} = Reader.peek(reader)
+    assert word == ")"
+    assert reader.position == 8
+    {reader,word} = Reader.next(reader)
+    assert word == ")"
+    assert reader.position == 9
+    {reader,word} = Reader.peek(reader)
+    assert word == nil
+    assert reader.position == 9
+
+    {reader,word} = Reader.next(reader)
+    assert word == nil
+    assert reader.position == 9
   end
 end
